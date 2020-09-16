d7efe9f5bac6615f96a4242e0a90c61a89206f74
Giovanni Santinelli <37382977+GiovanniSantinelli@users.noreply.github.com>
2020-09-16 20:14:29 +0200

    Update Towers.cpp

--- Towers/Towers.cpp
+++ Towers/Towers.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-
+/*
+Since we are not actually instered in the values of the "bars" but only on how many towers we have and the height of the largest tower, using a map we can store each number one
+at a time using the value as the key and the number it has been repeted as the value. Before inserting the number we check if it's already there so we need to scan the keys,
+that costs O(1) in the average case. We also keep track of the max bar height just checking each time we try to insert an element that is already in, if it's "height" is 
+larger than the maximum one. At the end we return the  max value and the size of the map, the total cost should be linear.
+*/
 using namespace std;
 
 vector<int> tower(vector<int> vec){
